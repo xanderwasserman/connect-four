86c8147d4a3849552f0ce8423e1a4273afad429e
Alexander Wasserman <xanderwasserman@gmail.com>
2025-02-25 19:04:45 +0100

    Added GamePiece enums

---
+++ connect_four/defines.py
@@ -0,0 +1,9 @@
+# defines.py
+# Alexander Wasserman
+# 25 Feb 2025
+from enum import Enum
+
+class GamePieceType(Enum):
+    NO_PIECE        = 'NONE'
+    RED_PIECE       = 'RED'
+    YELLOW_PIECE    = 'YELLOW'
