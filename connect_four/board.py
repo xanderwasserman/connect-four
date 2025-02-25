b8bfc088d02439f32284c66b9dfc21e8eb6a34c8
Alexander Wasserman <xanderwasserman@gmail.com>
2025-02-25 21:47:53 +0100

    Completed Board implementation

--- connect_four/board.py
+++ connect_four/board.py
@@ -1,20 +1,21 @@
 # board.py
 # Alexander Wasserman
 # 25 Feb 2025
-
-import random
-
-from defines import GamePieceType
+from typing import Tuple, Optional
+from connect_four.defines import GamePieceType
+from hmi.cli_interface import show_board
 
 class Board:
     ROWS = 6
     COLS = 7
     
+    
     def __init__(self):
         # 6x7 grid
         self.grid = [ [GamePieceType.NO_PIECE for _ in range(self.COLS)] for _ in range(self.ROWS)]
         
-    def place_piece(self, col: int, piece_type: GamePieceType):
+        
+    def place_piece(self, col: int, piece_type: GamePieceType) -> Optional[Tuple[int, int]]:
         '''
         Places the game piece in the specified column (0 - COLS).
         Returns the (row, col) where it was placed, or None if invalid or full.
@@ -29,7 +30,8 @@ class Board:
             
         return None # No opening was found, the column is full
     
-    def is_full(self):
+    
+    def is_full(self) -> bool:
         '''
         Check if the board is completely full (all grid elements occupied by a colour).
         '''
@@ -39,13 +41,14 @@ class Board:
                     return False
         return True
     
-    def check_for_winner(self, row: int, col:int, winning_number: int):
+    
+    def check_for_winner(self, row: int, col:int, winning_number: int) -> bool:
         '''
         Check a grid position if there is a winning line of colours.
         '''
         piece_type = self.grid[row][col]
         
-        if winning_number <= 0 or winning_number > max(row, col):
+        if winning_number <= 0 or winning_number > max(self.ROWS, self.COLS):
             return False # Invalid winning number
         
         if piece_type is GamePieceType.NO_PIECE:
@@ -62,26 +65,34 @@ class Board:
         for direction in directions:
             total_in_line = 1 # First element is the current game piece
             
-            for (delta_row, delta_column) in direction:
-                total_in_line += self.count_pieces_in_direction(row, col, delta_row, delta_column, piece_type)
+            for (delta_row, delta_col) in direction:
+                total_in_line += self.count_pieces_in_direction(row, col, delta_row, delta_col, piece_type)
                 
             if total_in_line >= winning_number:
                 return True # Current move is a winning move
             
         return False # No winners
     
-    def count_pieces_in_direction(self, row: int, col: int, delta_row: int, delta_column: int, piece_type: GamePieceType):
+    
+    def count_pieces_in_direction(self, row: int, col: int, delta_row: int, delta_col: int, piece_type: GamePieceType):
         '''
-        Count the number of equal type pieces in a specific direction indicated by (delta_row, delta_column)
+        Count the number of equal type pieces in a specific direction indicated by (delta_row, delta_col)
         '''
         count = 0
         
         row = row+delta_row
-        column = column+delta_column
+        col = col+delta_col
         
-        while 0 <= row < self.ROWS and 0 <= column < self.COLS and self.grid[row][column] == GamePieceType:
+        while 0 <= row < self.ROWS and 0 <= col < self.COLS and self.grid[row][col] == piece_type:
             count += 1
             row += delta_row
-            column += delta_column
+            col += delta_col
         
         return count
+    
+    
+    def display_board(self) -> None:
+        '''
+        Displays the board to the user
+        '''
+        show_board(self.ROWS, self.COLS, self.grid)
