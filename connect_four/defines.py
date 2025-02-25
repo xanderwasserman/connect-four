40a2718f39a3b45bc74ef3a44a4713b9ac080e0d
Alexander Wasserman <xanderwasserman@gmail.com>
2025-02-25 21:48:33 +0100

    Created defines for different enums

--- connect_four/defines.py
+++ connect_four/defines.py
@@ -2,8 +2,16 @@
 # Alexander Wasserman
 # 25 Feb 2025
 from enum import Enum
+import colorama
+
+# Initialize colorama (needed on Windows)
+colorama.init()
 
 class GamePieceType(Enum):
-    NO_PIECE        = 'NONE'
-    RED_PIECE       = 'RED'
-    YELLOW_PIECE    = 'YELLOW'
+    NO_PIECE     = ' '
+    RED_PIECE    = f'{colorama.Fore.RED}O{colorama.Style.RESET_ALL}'
+    YELLOW_PIECE = f'{colorama.Fore.YELLOW}O{colorama.Style.RESET_ALL}'
+    
+class GamePlayerType(Enum):
+    HUMAN           = 1
+    MACHINE         = 2
